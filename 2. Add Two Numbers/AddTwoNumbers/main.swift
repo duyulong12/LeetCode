d1b51ba4ca42d98ce78c14a6011a5f1f3b0e1369
duyulong <duyulong@simpletour.com>
2017-06-28 18:29:47 +0800

    optimizing arithmetic

--- 2. Add Two Numbers/AddTwoNumbers/main.swift
+++ 2. Add Two Numbers/AddTwoNumbers/main.swift
@@ -47,59 +47,77 @@ public class ListNode {
     }
 }
 
+/*
+ ListNode c1 = l1;
+ ListNode c2 = l2;
+ ListNode sentinel = new ListNode(0);
+ ListNode d = sentinel;
+ int sum = 0;
+ while (c1 != null || c2 != null) {
+ sum /= 10;
+ if (c1 != null) {
+ sum += c1.val;
+ c1 = c1.next;
+ }
+ if (c2 != null) {
+ sum += c2.val;
+ c2 = c2.next;
+ }
+ d.next = new ListNode(sum % 10);
+ d = d.next;
+ }
+ if (sum / 10 == 1)
+ d.next = new ListNode(1);
+ return sentinel.next;
+ 
+ */
+
 class Solution {
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
         let node:ListNode = ListNode(0);
-        var lNum:Int = 0;
-        var rNum:Int = 0;
+        var sum:Int = 0;
+        var temp:ListNode = node;
         var hl:ListNode? = l1;
         var hr:ListNode? = l2;
         
-        while (hl != nil) {
-            lNum += (hl?.val ?? 0);
-            lNum *= 10;
-            hl = hl?.next;
-        }
-        lNum /= 10;
-        
-        while (hr != nil) {
-            rNum += (hr?.val ?? 0);
-            rNum *= 10;
-            hr = hr?.next;
-        }
-        rNum /= 10;
-        
-        let value:Int = lNum + rNum;
-       
-        var v:Int = value;
-        
-        var tn:ListNode = node;
-        while (v != 0) {
-            tn.val = v % 10;
-            v /= 10;
-            
-            if(v != 0) {
-                tn.next = ListNode(0);
-                tn = tn.next!;
+        while (hl != nil || hr != nil) {
+            sum /= 10;
+            if (hl != nil) {
+                sum += hl?.val ?? 0;
+                hl = hl!.next;
             }
+            
+            
+            if (hr != nil) {
+                sum += hr?.val ?? 0;
+                hr = hr!.next;
+            }
+            
+            temp.next = ListNode(sum % 10);
+            temp = temp.next!;
         }
-        return node;
+        
+        if (sum / 10 == 1) {
+            temp.next = ListNode(1);
+        }
+        
+        return node.next;
     }
 }
 
 let solution:Solution = Solution();
-//let l1:ListNode = ListNode(2);
-//l1.next = ListNode(4);
-//l1.next?.next = ListNode(3);
-//
-//let l2:ListNode = ListNode(5);
-//l2.next = ListNode(6);
-//l2.next?.next = ListNode(4);
+let l1:ListNode = ListNode(2);
+l1.next = ListNode(4);
+l1.next?.next = ListNode(3);
 
-let l1:ListNode = ListNode(1);
-//l1.next = ListNode(8);
-let l2:ListNode = ListNode(9);
-l2.next = ListNode(9);
+let l2:ListNode = ListNode(5);
+l2.next = ListNode(6);
+l2.next?.next = ListNode(4);
+
+//let l1:ListNode = ListNode(1);
+////l1.next = ListNode(8);
+//let l2:ListNode = ListNode(9);
+//l2.next = ListNode(9);
 
 var node:ListNode? = solution.addTwoNumbers(l1, l2);
 
