7c953f3ca920058e18eb584a4af37ddc65c23c3b
duyulong <duyulong@simpletour.com>
2017-06-28 18:32:03 +0800

    optimizing arithmetic

--- 2. Add Two Numbers/AddTwoNumbers/main.swift
+++ 2. Add Two Numbers/AddTwoNumbers/main.swift
@@ -47,30 +47,6 @@ public class ListNode {
     }
 }
 
-/*
- ListNode c1 = l1;
- ListNode c2 = l2;
- ListNode sentinel = new ListNode(0);
- ListNode d = sentinel;
- int sum = 0;
- while (c1 != null || c2 != null) {
- sum /= 10;
- if (c1 != null) {
- sum += c1.val;
- c1 = c1.next;
- }
- if (c2 != null) {
- sum += c2.val;
- c2 = c2.next;
- }
- d.next = new ListNode(sum % 10);
- d = d.next;
- }
- if (sum / 10 == 1)
- d.next = new ListNode(1);
- return sentinel.next;
- 
- */
 
 class Solution {
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
